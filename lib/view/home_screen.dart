667fd652c260098b9c2a99b7c43643d6feb50564
Chamroeun Sithy <90774658+ChamroeunSithy@users.noreply.github.com>
2025-08-01 20:58:16 +0700

    Update home_screen.dart

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 builder: (context) {
                   return AlertDialog(
                     title: const Text('Notifications'),
-                    content: const Text('No notifications'),
+                    content: const Text('No notifications168'),
                   );
                 },
               );
