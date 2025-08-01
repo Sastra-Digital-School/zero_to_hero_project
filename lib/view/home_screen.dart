5a3094f41846fac8cd122aed25165b274486c948
Macbook Pro Sithy <macbookpro@Mac.lan>
2025-08-01 20:39:00 +0700

    update on home screen and setting screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [Home(), Settings()];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
-      body: Center(child: Text('Body')),
+      body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
@@ -18,5 +35,23 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Home'));
+  }
+}
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Settings'));
   }
 }
--- lib/view/setting_screen.dart
+++ lib/view/setting_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool _isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +19,73 @@ class SettingScreen extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.language),
             title: Text('Language'),
-            onTap: () {},
+            onTap: () {
+              // navigate to language setting screen
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => LanguageSettingScreen(),
+                ),
+              );
+            },
           ),
           ListTile(
             leading: Icon(Icons.brightness_6),
             title: Text('Theme'),
+            trailing: Switch(
+              value: _isDarkMode,
+              onChanged: (bool value) {
+                setState(() {
+                  _isDarkMode = value;
+                });
+              },
+            ),
             onTap: () {
-              // navigate to theme setting screen
+              // toggle theme
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class LanguageSettingScreen extends StatefulWidget {
+  const LanguageSettingScreen({super.key});
+
+  @override
+  State<LanguageSettingScreen> createState() => _LanguageSettingScreenState();
+}
+
+class _LanguageSettingScreenState extends State<LanguageSettingScreen> {
+  final List<Locale> _locales = [Locale('en', 'US'), Locale('zh', 'CN')];
+
+  Locale? _selectedLocale;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Language')),
+      body: ListView.builder(
+        itemCount: _locales.length,
+        itemBuilder: (context, index) {
+          final locale = _locales[index];
+          return RadioListTile<Locale>(
+            value: locale,
+            groupValue: _selectedLocale,
+            onChanged: (value) {
+              setState(() {
+                _selectedLocale = value;
+              });
+            },
+            title: Text('${locale.languageCode}_${locale.countryCode}'),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // set selected locale to system
+        },
+        child: Icon(Icons.check),
       ),
     );
   }
